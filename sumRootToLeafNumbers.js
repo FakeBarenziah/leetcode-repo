659f8c985e02adba6cc29aad06e78497f6667447
Nick B <nicholas.balestrieri@gmail.com>
2019-10-05 17:35:11 -0400

    Sum Root To Leaf Numbers solution

---
+++ sumRootToLeafNumbers.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumNumbers = function(root) {
+  let total = 0;
+
+  if (!root) return total;
+
+  const treeTraverse = (node, path = []) => {
+    const newPath = [...path];
+    newPath.push(node.val);
+    if (node.left) treeTraverse(node.left, newPath);
+    if (node.right) treeTraverse(node.right, newPath);
+    if (!node.left && !node.right) total += Number(newPath.join(""));
+  };
+  treeTraverse(root);
+  return total;
+};

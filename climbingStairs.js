5d1fa3ec2adee283f8c9f622bdaeb8050ece8d9a
Nick B <nicholas.balestrieri@gmail.com>
2019-11-08 15:22:38 -0500

    add dynamic programming solution for Climbing Stairs

--- climbingStairs.js
+++ climbingStairs.js
@@ -3,15 +3,14 @@
  * @return {number}
  */
 
-const recurse = (i, n, memo) => {
-  if (i > n) return 0;
-  if (i === n) return 1;
-  if (memo[i] > 0) return memo[i];
-  memo[i] = recurse(i + 1, n, memo) + recurse(i + 2, n, memo);
-  return memo[i];
-};
-
 var climbStairs = function(n) {
-  const memo = [];
-  return recurse(0, n, memo);
+  if (n === 1) return 1;
+  const dp = new Array(n);
+  dp[1] = 1;
+  dp[2] = 2;
+
+  for (let i = 3; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+  return dp[n];
 };

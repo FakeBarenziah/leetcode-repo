ee0e00c3a822c851e69e740a1abde73c1efc0bf5
Nick B <nicholas.balestrieri@gmail.com>
2019-11-17 21:07:18 -0500

    add solution for Remove Duplicates from Sorted List II using recursion

--- removeDuplicatesFromSortedListII.js
+++ removeDuplicatesFromSortedListII.js
@@ -9,25 +9,16 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var deleteDuplicates = function(head) {
-  let falseHead = new ListNode(false);
-  falseHead.next = head;
-  // a pointer which never includes duplicates on its left
-  let pointer = falseHead;
-  let dupeFinder = pointer.next;
+var deleteDuplicates = head => {
+  if (head === null || head.next === null) return head;
 
-  while (dupeFinder) {
-    if (dupeFinder.next && dupeFinder.val === dupeFinder.next.val) {
-      let dupeVal = dupeFinder.val;
-      while (dupeFinder && dupeFinder.val === dupeVal) {
-        dupeFinder = dupeFinder.next;
-        if (!dupeFinder) pointer.next = dupeFinder;
-      }
-    } else {
-      pointer.next = dupeFinder;
-      pointer = pointer.next;
-      dupeFinder = pointer.next;
+  if (head.val !== head.next.val) {
+    head.next = deleteDuplicates(head.next);
+    return head;
+  } else {
+    while (head.next !== null && head.val === head.next.val) {
+      head = head.next;
     }
+    return deleteDuplicates(head.next);
   }
-  return falseHead.next;
 };

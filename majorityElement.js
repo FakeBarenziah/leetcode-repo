72c2a4c2426781be7bd8d7da916100f43ad792fb
Nick B <nicholas.balestrieri@gmail.com>
2019-11-19 00:14:24 -0500

    adds solution for Majority Element using Boyer-Moore Voting algorithm

--- majorityElement.js
+++ majorityElement.js
@@ -3,12 +3,13 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-  const threshold = Math.floor(nums.length / 2) + 1;
-  const values = {};
-  let retVal;
-  nums.forEach(each => {
-    values[each] ? values[each]++ : (values[each] = 1);
-    if (values[each] >= threshold) retVal = each;
+  let count = 0;
+  let candidate = null;
+
+  nums.forEach(num => {
+    if (count === 0) candidate = num;
+
+    count += num === candidate ? 1 : -1;
   });
-  return retVal;
+  return candidate;
 };

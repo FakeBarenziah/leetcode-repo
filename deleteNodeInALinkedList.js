e0b25913b5482212834635d022aef4dd58aece45
Nick B <nicholas.balestrieri@gmail.com>
2019-11-24 23:50:17 -0500

    add improved solution for Delete Node in a Linked List

--- deleteNodeInALinkedList.js
+++ deleteNodeInALinkedList.js
@@ -10,12 +10,6 @@
  * @return {void} Do not return anything, modify node in-place instead.
  */
 var deleteNode = function(node) {
-  while (node.next) {
-    node.val = node.next.val;
-    if (!node.next.next) {
-      node.next = null;
-      break;
-    }
-    node = node.next;
-  }
+  node.val = node.next.val;
+  node.next = node.next.next;
 };

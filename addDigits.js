be1b7d1b62e0f2fdb24a823f9eaf85c7436420eb
Nick B <nicholas.balestrieri@gmail.com>
2019-11-28 20:40:36 -0500

    add solution for Add Digits using O(1) time and space implemetation

--- addDigits.js
+++ addDigits.js
@@ -3,20 +3,5 @@
  * @return {number}
  */
 var addDigits = function(num) {
-  num = String(num)
-    .split("")
-    .map(e => Number(e));
-  let total = 0;
-  let pointer = 0;
-
-  while (pointer < num.length) {
-    let subtotal = total + num[pointer];
-    if (subtotal > 9) {
-      subtotal -= 10;
-      num.push(1);
-    }
-    total = subtotal;
-    pointer++;
-  }
-  return total;
+  return 1 + ((num - 1) % 9);
 };
